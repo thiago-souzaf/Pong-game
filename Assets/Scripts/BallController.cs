b15a1cd7338b3dbbe28098b9208a5ffedcfa5639
Thiago de Souza Farias <tsf@cin.ufpe.br>
2023-10-15 18:09:49 -0300

    add: Countdown before start
    implements a countdown animation before starting the game, and before resets

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float speed;
+    public float minDirection = 0.5f;
+
+    private Vector3 direction;
+    private Rigidbody rb;
+
+    private bool stopped = true;
+
+    private GameController gameController;
+
+    public GameObject downBorder;
+    public GameObject upBorder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        if (stopped) return;
+
+        this.rb.MovePosition(this.rb.position + direction * speed * Time.deltaTime);
+        if(this.transform.position.z > upBorder.transform.position.z || this.transform.position.z < downBorder.transform.position.z)
+        {
+            
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            direction.z = -direction.z;
+        }
+
+        if (other.CompareTag("Player"))
+        {
+            Vector3 newDirection = (transform.position - other.transform.position).normalized;
+
+
+            newDirection.x = Mathf.Sign(newDirection.x) * Mathf.Max(Mathf.Abs(newDirection.x), this.minDirection);
+            newDirection.z = Mathf.Sign(newDirection.z) * Mathf.Max(Mathf.Abs(newDirection.z), this.minDirection);
+
+            direction = newDirection;
+        }
+    }
+
+    private void ChooseDirection()
+    {
+        float signX = Mathf.Sign(Random.Range(-1f, 1f));
+        float signY = Mathf.Sign(Random.Range(-1f, 1f));
+        this.direction = new Vector3(0.5f * signX, 0, 0.5f * signY);
+    }
+    
+    public void Stop()
+    {
+        this.stopped = true;
+    }
+    public void Go()
+    {
+        ChooseDirection();
+        this.stopped = false;
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,19 +6,42 @@ using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject ball;
+
     public TextMeshProUGUI scoreText;
     private int scoreLeft = 0;
     private int scoreRight = 0;
+
+    private bool started = false;
+
+    private BallController ballController;
+
+    private Vector3 startingPosition;
+
+    public Starter starter;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.ballController = ball.GetComponent<BallController>();
+        this.startingPosition = this.ball.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.started) return;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.started = true;
+            this.starter.StartCountdown();
+        }
+    }
+
+    public void StartGame()
+    {
+        this.ballController.Go();
     }
 
     public void ScoreGoalLeft()
@@ -26,6 +49,7 @@ public class GameController : MonoBehaviour
         Debug.Log("Gol na esquerda");
         this.scoreRight++;
         UpdateUI();
+        ResetBall();
     }
 
     public void ScoreGoalRight()
@@ -33,10 +57,19 @@ public class GameController : MonoBehaviour
         Debug.Log("Gol na direita");
         this.scoreLeft++;
         UpdateUI();
+        ResetBall();
     }
 
     private void UpdateUI()
     {
-        this.scoreText.text = $"{scoreLeft.ToString()} x {scoreRight.ToString()}";
+        this.scoreText.text = $"{scoreLeft.ToString()} x {scoreRight}";
     }
+
+    public void ResetBall()
+    {
+        this.ballController.Stop();
+        this.ball.transform.position = this.startingPosition;
+        this.starter.StartCountdown();
+    }
+
 }
